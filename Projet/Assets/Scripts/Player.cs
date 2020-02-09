20ef03719eb85f8d3b0a23fbea253c98c61ea074
Guillaume <guillaumeloux@gmail.com>
2020-02-09 19:45:30 +0100

    Nothing special

---
+++ Projet/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 5F;
+
+	public Rigidbody2D rigidbody;
+
+	Vector2 movement;
+
+    void Start()
+    {	
+
+    }
+
+	void Update(){
+		movement.x = Input.GetAxisRaw("Horizontal");
+		movement.y = Input.GetAxisRaw("Vertical");
+
+	}
+
+    void FixedUpdate()
+    {	
+        rigidbody.MovePosition(rigidbody.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
