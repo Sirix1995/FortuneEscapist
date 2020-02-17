6d2ac4e53372582759f235e47de81ac13907a7c9
Jb <jeanbaptiste.calendini@gmail.com>
2020-02-17 15:11:03 +0100

    debug

--- Projet/Assets/Scripts/MainMenu.cs
+++ Projet/Assets/Scripts/MainMenu.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
@@ -49,12 +50,19 @@ public class MainMenu : MonoBehaviour
         entryTemplate = entryContainer.Find("highscoreEntryTemplate");
 
         entryTemplate.gameObject.SetActive(false);
-        float templateHeight = 40f;
+        float templateHeight = 80f;
         for (int i = 0; i < 10; i++){
             Transform entryTransform = Instantiate(entryTemplate, entryContainer);
             RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
             entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
             entryTransform.gameObject.SetActive(true);
+
+            int rank = i + 1;
+            int score = Random.Range(0, 10000);
+
+            entryTransform.Find("posText").GetComponent<TextAlignment>().text = rank.ToString();
+            entryTransform.Find("nameText").GetComponent<TextAlignment>().text = score.ToString();
+            entryTransform.Find("posText").GetComponent<TextAlignment>().text = "AAA";
         }
 
 
